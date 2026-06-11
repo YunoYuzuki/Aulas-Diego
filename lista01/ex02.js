390ad76856e33532fbceaaf06a072b929a6e57b4
YunoYuzuki <yunos90a@gmail.com>
2026-06-11 08:50:27 -0300

    Lista 01

--- lista01/ex02.js
+++ lista01/ex02.js
@@ -4,7 +4,7 @@ let b = Number(input.question("Qual o tamanho da base? "))
 let a = Number(input.question("Qual o tamanho da altura? "))
 
 function perimetro(a, b){
-    return (a*2) + (b*2);
+    return 2*(a + b);
 }
 
 console.log(perimetro(a, b));
--- lista01/ex05.js
+++ lista01/ex05.js
@@ -1,12 +1,21 @@
 const input = require('readline-sync')
 
-let num = 1
-
-function cont(num){
-    return num + 1;
-}
-console.log("1");
+let num = 0
 
 for(let i = 0; i <= 99; i++){
-    console.log(cont(num))
+    num++
+
+    if(num %3 === 0){
+        console.log(`Fizz`);
+    }
+    else if(num %5 === 0){
+        console.log(`Buzz`);
+    }
+    if(num %3 === 0 && num %5 === 0){
+        console.log(`FizzBuzz`);
+    }
+
+    else {
+        console.log(`${num}`)
+    }
 }
